6e49c7ef0b8a475434abae4e3d7070749fdffca0
LincCodes <linccodes@gmail.com>
2021-02-18 17:18:31 +0200

    Added now content

--- pages/linccodes.js
+++ pages/linccodes.js
@@ -10,7 +10,7 @@ const bucket = api.bucket({
 export default function Home({ blogs }) {
    return (
     <div>
-      <div className="heading text-center font-medium text-2xl m-3 text-gray-100">BLOGS</div>
+      <div className="heading text-center font-medium text-2xl m-3 text-gray-100">EDIT YOUR CONTENT</div>
       <div className="holder mx-auto grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
         
         {
@@ -21,11 +21,9 @@ export default function Home({ blogs }) {
               <h1 className="text-gray-900 font-bold text-2xl uppercase">{blog.title}</h1>
               <p className="text-gray-600 text-sm my-2 h-10 overflow-hidden">{blog.metadata.summary}</p>
             </div>
-            <img className="h-56 w-full object-cover mt-2" src={blog.metadata.img.imgix_url} alt="NIKE AIR"/>
             <div className="flex items-center justify-between px-4 py-2 bg-gray-900">
-              <p className="text-gray-200 font-bold text">{new Date(blog.created).toLocaleDateString()}</p>
-              <Link href="/blog/[id]" as={`/blog/${blog.slug}`} >
-              <a className="px-3 py-1 bg-gray-200 text-sm text-gray-900 font-semibold rounded hover:bg-gray-400">Read more</a>
+              <Link href="/edit/[id]" as={`/edit/${blog.slug}`} >
+              <a className="px-3 py-1 bg-gray-200 text-sm bg-green-900 font-semibold rounded hover:bg-green-600">Edit</a>
               </Link>
             </div>
             </div>
@@ -41,7 +39,7 @@ export default function Home({ blogs }) {
 export async function getServerSideProps() {
   const data = await bucket.getObjects({
     type: 'blogs',
-    props: 'slug,title,metadata.img.imgix_url,metadata.summary,created' // Limit the API response data by props
+    props: 'slug,title,metadata.summary' // Limit the API response data by props
   })
   const blogs = await data.objects
   return {
